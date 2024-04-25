0687fcff949823e8e5f2bf5f013f109576d116e1
jungsu13 <olympos13@naver.com>
2024-04-25 17:04:52 +0900

    0425

---
+++ a-3/main.js
@@ -0,0 +1,39 @@
+$(function(){
+    // gnb
+    $(".gnb li").hover(
+        function() {            
+            $(this).find(".sub").stop().slideDown()
+        },
+        function() {            
+            $(this).find(".sub").stop().slideUp()
+        }
+    )      
+
+    // modal
+    $("#showModal").click(function(){
+        $(".modal").show()
+    })
+
+    $("#closeModal").click(function(){
+        $(".modal").hide()
+    })
+
+    // slide
+    setInterval(function(){
+        $(".slides").animate({left: "-100%"}, function(){
+            $(this).append($(".slide").eq(0))
+            $(this).css("left", 0)
+        })
+    }, 3000)
+
+    // tab
+    $(".tab_title h3").click(function(){
+        $(".tab_title h3").removeClass("active")
+        $(this).addClass("active")
+
+        var idx = $(this).index()
+        $(".tab_contents .item").hide()
+        $(".tab_contents .item").eq(idx).show()
+    })   
+
+}) 
